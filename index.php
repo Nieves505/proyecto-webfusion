a27a6c1779a79c5019103358c099ddc25b6be5f5
Nieves505 <nievesteniola@gmail.com>
2026-05-06 09:57:21 +0000

    Cambio

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-Bienvenid@ a WebFusion Digital
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>WebFusion Digital</title>
+</head>
+<body>
+
 <?php
 echo "<h1>WebFusion Digital</h1>";
-echo "<p>Proyecto </p>";
+echo "<p>Proyecto</p>";
+echo "<h2>Prueba1</h2>";
 ?>
-<h2>Prueba1</h2>
+
+</body>
+</html>
